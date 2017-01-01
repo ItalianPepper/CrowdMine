1923aae0fb4ceb11e9690446dbdd91ceb2df0e3e
Dario Galiani <thedarkvipo@gmail.com>
2017-01-01 17:26:39 +0100

    Implementata funzione di pool per l'header

--- style/assets/js/notifyPanelFunction.js
+++ style/assets/js/notifyPanelFunction.js
@@ -9,24 +9,8 @@ $(function poll(){
             url: "pannelloNotifiche",
             dataType: "json",
             success: function(data){
-                var parsed = JSON.parse(data);
-                var lista = [];
-                for(var x in parsed){
-                    lista.push(x);
-                }
                 var listaNotifiche = document.getElementById('lista-notifiche');
-                $.each(lista, function(value, key){
-                listaNotifiche.append(
-                '<li>'+
-                '<a href="'+ value +'">'+
-                    '<div class="message">'+
-                    '<div class="content">'+
-                    '<div class="title">'+ key +'</div>'+
-                '</div>'+
-                '</div>'+
-                '</a>'+
-                '</li>')
-                });
+                generateNotificationsList(data, listaNotifiche);
                 poll();
             }
         });
@@ -35,9 +19,16 @@ $(function poll(){
 
 function generateNotificationsList(data, destination){
     if(data.length > 0){
-        for(var i in data){
-            var notifica = [];
+        $.each(data, function (key, value) {
+            var listaNotificheObject = [];
+            listaNotificheObject.href = key;
+            listaNotificheObject.corpo = value;
 
-        }
+            destination.children(':first').before(notificaToRowString(listaNotificheObject));
+        });
     }
+}
+
+function notificaToRowString(listaNotificheObject){
+    return '<li><a href="'+listaNotificheObject.href+'">'+listaNotificheObject.corpo+'</a></li>';
 }
