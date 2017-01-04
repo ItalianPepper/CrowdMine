f9852a6c2b707718375d9d6599918cf99569e58f
Angelo Caputo <a.caputo1991@gmail.com>
2017-01-04 17:46:34 +0100

    aggiunto file per test di annuncio manager

---
+++ core/tests/test manager/AnnuncioManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Angelo
+ * Date: 04/01/2017
+ * Time: 17:40
+ */
+require_once(__DIR__ .  '/../../manager/AnnuncioManager.php');
+class AnnuncioManagerTest extends PHPUnit_Framework_TestCase
+{
+
+}
