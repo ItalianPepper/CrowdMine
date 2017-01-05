5bc8f0ce9c506c974849b3e19a0237ed38ac3ac3
Angelo Caputo <a.caputo1991@gmail.com>
2017-01-05 01:55:34 +0100

    Test di un metodo del gestore annunci

--- core/tests/test manager/AnnuncioManagerTest.php
+++ core/tests/test manager/AnnuncioManagerTest.php
@@ -6,8 +6,24 @@
  * Date: 04/01/2017
  * Time: 17:40
  */
-require_once(__DIR__ .  '/../../manager/AnnuncioManager.php');
+include_once(__DIR__ .  '/../../manager/AnnuncioManager.php');
+include_once(__DIR__ .  '/../../manager/Manager.php');
+include_once(__DIR__ .  '/../../Config.php');
+include_once(__DIR__ .  '/../../filter/SearchByIdFilter.php');
+include_once(__DIR__ .  '/../../filter/FilterUtils.php');
+include_once(__DIR__ .  '/../../model/Annuncio.php');
+
+
 class AnnuncioManagerTest extends PHPUnit_Framework_TestCase
 {
+    protected $annuncioManager;
 
+    public function setUp(){
+        $this->annuncioManager = new AnnuncioManager();
+    }
+
+    public function testGetAnnuncio(){
+        $annuncio = $this->annuncioManager->getAnnuncio(1);
+        self::assertEquals("Annuncio1",$annuncio->getTitolo());
+    }
 }
