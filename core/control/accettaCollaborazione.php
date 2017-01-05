63dbfe2c2fecf692ded0acf3a599f064160adf67
Dario Galiani <thedarkvipo@gmail.com>
2017-01-05 08:16:39 +0100

    Fix. Eliminato la gestione dell'utente mediante variabile Session in favore del metodo checkPermission

--- core/control/accettaCollaborazione.php
+++ core/control/accettaCollaborazione.php
@@ -1,30 +1,20 @@
 <?php
-    
-    include_once MODEL_DIR . "Utente.php";
-    include_once MODEL_DIR . "Messaggio.php";
-    include_once MODEL_DIR . "Candidatura.php";  
-    include_once MANAGER_DIR . "MessaggioManager.php";
- 
-    
-    ## RECUPERO INFORMAZIONI SULL'UTENTE CONNESSO ##
-    // session_start();
-    // $utente = $_SESSION['utente'];
-    //$_SESSION['lista']= serialize($lista-utenti);
-    $utente_connesso = new Utente(0, 'Alfredo', 'Fiorillo', "38093", "Sal", "aprile", "alfred.fiorillo@gmail.com", "password", "stato", "amministratore", "immagine" );
-    // if ($utente == null)
-    //     header("location:./index.php");
-    
 
-    
-    $id_candidatura = $_POST["id"];
-    $idDestinatario = $_SESSION['destinatario'];
-    ## MANAGER ##
-    $manager_msg = new MessaggioManager();
-     
-    ## RECUPERO IL  DELLA CONVERSAZIONE ###
-    $invio_candidatura = $manager_msg->setAccettaCollaborazione($id_candidatura);  //[STUB getUtentebyID]
-    $manager_msg->sendMessaggio(null, "[COLLABORAZIONE ACCETTATA]", '', '', $utente_connesso->getId(), $idDestinatario);
-  
-    if($invio_candidatura){
-        include_once CONTROL_DIR . "stampaCandidature.php";
-    }
+include_once MODEL_DIR . "Utente.php";
+include_once MODEL_DIR . "Messaggio.php";
+include_once MODEL_DIR . "Candidatura.php";
+include_once MANAGER_DIR . "MessaggioManager.php";
+
+
+$id_candidatura = $_POST["id"];
+$idDestinatario = $_SESSION['destinatario'];
+## MANAGER ##
+$manager_msg = new MessaggioManager();
+
+## RECUPERO IL  DELLA CONVERSAZIONE ###
+$invio_candidatura = $manager_msg->setAccettaCollaborazione($id_candidatura);  //[STUB getUtentebyID]
+$manager_msg->sendMessaggio(null, "[COLLABORAZIONE ACCETTATA]", '', '', $user->getId(), $idDestinatario);
+
+if($invio_candidatura){
+    include_once CONTROL_DIR . "stampaCandidature.php";
+}
