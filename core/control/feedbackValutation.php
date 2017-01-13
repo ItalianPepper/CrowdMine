5c4322bf86fe5e929aff9fa0e9001c62bcd39608
GiovanniLeo <johnny.1995@hotmail.it>
2017-01-13 22:49:48 +0100

    Aggiunti i controlli mancanti a segnalazione feedback

--- core/control/feedbackValutation.php
+++ core/control/feedbackValutation.php
@@ -7,19 +7,27 @@
  */
 include_once MANAGER_DIR . 'FeedbackManager.php';
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $feedback_manager = new FeedbackManager();
-    if(isset($_POST["id"]) && isset($_POST["stato"])){
-        $feedback_manager->setStatus($_POST["id"],$_POST["stato"]);
-        $return =array(
-            'toastType' => "success",
-            'toastMessage' => "Feedback risolto con successo"
-        );
+    if (isset($_POST["id"]) && isset($_POST["stato"])) {
+        $feedback = $feedback_manager->getFeedbackById($_POST["id"]);
+        if ($feedback->getStato() == ELIMINATO || $feedback->getStato() == AMMINISTRATORE ) {
+            $return = array(
+                'toastType' => "error",
+                'toastMessage' => "Il feedback non è più disponibile o lo sta analizzando un amministratore"
+            );
+        } else {
+            $feedback_manager->setStatus($_POST["id"], $_POST["stato"]);
+            $return = array(
+                'toastType' => "success",
+                'toastMessage' => "Feedback risolto con successo"
+            );
+        }
         echo json_encode($return);
-    }
-    else{
-        $return =array(
+
+    } else {
+        $return = array(
             'toastType' => "error",
             'toastMessage' => "Problema con la risoluzione del Feedback"
         );
