576a174fcb2af286d9ba344e74dff8f4d8c8b68a
GiovanniLeo <johnny.1995@hotmail.it>
2017-01-16 12:16:11 +0100

    Apportate modifiche alle reg exp

--- core/utils/Patterns.php
+++ core/utils/Patterns.php
@@ -8,7 +8,7 @@
 class Patterns {
     public static $EMAIL = "/^[a-zA-Z0-9+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}$/";
     public static $MATRICOLA = "/^[0-9]{2,12}$/";
-    public static $NAME_GENERIC = "^[a-zA-Z0-9_ èàòù,.]+$";
+    public static $NAME_GENERIC = "/^[a-zA-Z0-9_ èàòù,.]+$/";
     public static $TELEPHONE = "/^[0-9]{5,10}$/";
     public static $GENERIC_DATE = "/([0-9]{4}-[0-9]{2}-[0-9]{2})/";
     public static $MD5_SLASH = "/^[a-f0-9]{32}\|[a-f0-9]{32}$/";
